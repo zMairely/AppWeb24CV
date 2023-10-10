69367cf5dbf295dd90c09bf30177df0d76214aa2
zMairely <mairelypenaloza20@gmail.com>
2023-10-10 08:37:54 -0500

    Agregar archivos de proyecto.

---
+++ AppWeb24CV/Controllers/EjemploController.cs
@@ -0,0 +1,21 @@
+﻿using AppWeb24CV.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWeb24CV.Controllers
+{
+    public class EjemploController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contacto()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Moises";
+            persona.Apellidos = "Torres";
+            persona.FechaNacimiento = new DateTime(1982, 01, 15);
+            return View(persona);
+        }
+    }
+}
---
+++ AppWeb24CV/Models/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace AppWeb24CV.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
